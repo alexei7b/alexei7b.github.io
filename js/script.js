0f34f085d75f470bd6e6319f81b333a2737084a4
Alex <alexei_7b@mail.ru>
2026-07-15 13:08:06 +0300

    feat: add portfolio projects and contact form

--- js/script.js
+++ js/script.js
@@ -1,19 +1,50 @@
 const hamburger = document.querySelector('.hamburger'),
-        menu = document.querySelector('.menu'),
-        closeElem = document.querySelector('.menu__close');
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
+overlay = document.querySelector('.menu__overlay');
 
-hamburger.addEventListener('click', ()=>{
+hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
-closeElem.addEventListener('click', ()=>{
+closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+overlay.addEventListener('click', () => {
+    menu.classList.remove('active');
+})
 
 const counters = document.querySelectorAll('.skills__ratings-counter'),
-      lines = document.querySelectorAll('.skills__ratings-line span');
+    lines = document.querySelectorAll('.skills__ratings-line span');
 
 
-counters.forEach((item,i) => {
+counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
 });
 
+
+
+emailjs.init({
+    publicKey: "QiNsWYQ7gNsdDV_YL",
+});
+
+
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    emailjs.sendForm(
+        "service_3py76bg", // Service ID
+        "template_ejohnmj",
+        this
+    )
+        .then(() => {
+            alert("Message sent!");
+            form.reset();
+        })
+        .catch((error) => {
+            alert("Error");
+            console.log(error);
+        });
+
+});
